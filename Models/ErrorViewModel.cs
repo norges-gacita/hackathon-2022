977b2c25de4d6fce4b4c58844e18c75d489c9116
Norges Gacita <norges.gacita@pricemetrix.com>
2022-12-14 13:28:05 -0500

    Create Web App

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace hackathon_2022_platform.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
--- README.md
+++
@@ -1,4 +0,0 @@
-# hackathon-2022-platform
-# Test 01
-# Test 02
-# Test 03
